3dd613fce8beb3b2e2c8d0878ce16ce2b93b6ffe
Mariana Leitão <marianayanna@yahoo.com.br>
2023-03-21 15:00:07 -0300

    ...

--- projeto_univesp1/src/router.tsx
+++ projeto_univesp1/src/router.tsx
@@ -1,26 +1,15 @@
 
+import {BrowserRouter, Route } from 'react-router';
 
-import logo from './logo.svg';
-import './App.css';
+import Login from './pages/Login';
 
 function Routes () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Route path = '/' exact component = { < Login />} />
+      
+      </BrowserRouter>
+
   );
 }
 
